99cf100d5813bca702832dfe1efc0496791b5c53
IshikawaMasashi <IshikawaMasashi@outlook.com>
2022-10-23 11:35:26 +0900

    fix

--- src/Vector3.ts
+++ src/Vector3.ts
@@ -1,6 +1,4 @@
 ﻿import { MathHelper } from './MathHelper';
-import { Vector2 } from './Vector2';
-import { Quaternion } from './Quaternion';
 import { Matrix } from './Matrix';
 
 export class Vector3 {

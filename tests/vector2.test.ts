add45a92e0ac2bf822a7f0c816c624b50aa6aa1e
IshikawaMasashi <IshikawaMasashi@outlook.com>
2022-10-27 07:25:33 +0900

    fix

--- tests/vector2.test.ts
+++ tests/vector2.test.ts
@@ -4,7 +4,7 @@ import { Vector2 } from '../src';
 describe('Vector2', () => {
   it('初期化テスト', () => {
     const a = new Vector2();
-    expect(a.x).toEqual(0);
-    expect(a.y).toEqual(0);
+    expect(a.x).toBe(0);
+    expect(a.y).toBe(0);
   });
 });

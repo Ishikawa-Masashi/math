7861ca57635f4ba45a2275eb464def11a0186b25
IshikawaMasashi <IshikawaMasashi@outlook.com>
2022-10-26 12:58:36 +0900

    fix

--- src/Matrix.ts
+++ src/Matrix.ts
@@ -2259,7 +2259,7 @@ export class Matrix {
  * @internal
  * Same as Tmp but not exported to keep it only for math functions to avoid conflicts
  */
-export class MathTmp {
+class MathTmp {
   public static Vector3 = ArrayTools.BuildTuple(11, () => Vector3.Zero);
   public static Matrix = ArrayTools.BuildTuple(2, () => Matrix.Identity);
   public static Quaternion = ArrayTools.BuildTuple(3, () => Quaternion.Zero);

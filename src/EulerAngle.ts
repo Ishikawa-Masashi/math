9713b798c4e86ad6fc42ea3ef9af2d521a68e4c8
IshikawaMasashi <IshikawaMasashi@outlook.com>
2022-11-20 07:22:53 +0900

    fix

--- src/EulerAngle.ts
+++ src/EulerAngle.ts
@@ -41,6 +41,24 @@ export class EulerAngle {
       case EulerOrder.XYZ:
         return new Matrix(
           cy * cz,
+          sx * sy * cz + cx * sz,
+          -cx * sy * cz + sx * sz,
+          0,
+          -cy * sz,
+          -sx * sy * sz + cx * cz,
+          cx * sy * sz + sx * cz,
+          0,
+          sy,
+          -sx * cy,
+          cx * cy,
+          0,
+          0,
+          0,
+          0,
+          1
+        );
+        return new Matrix(
+          cy * cz,
           -cy * sz,
           sy,
           0,
@@ -61,6 +79,27 @@ export class EulerAngle {
           1
         );
       case EulerOrder.XZY:
+        return new Matrix(
+          cy * cz,
+          cx * cy * sz + sx * sy,
+          sx * cy * sz - cx * sy,
+          0,
+
+          -sz,
+          cx * cz,
+          sx * cz,
+          0,
+
+          sy * cz,
+          cx * sy * sz - sx * cy,
+          sx * sy * sz + cx * cy,
+          0,
+
+          0,
+          0,
+          0,
+          1
+        );
         return new Matrix(
           cy * cz,
           -sz,
@@ -85,6 +124,27 @@ export class EulerAngle {
       case EulerOrder.YXZ:
         return new Matrix(
           sx * sy * sz + cy * cz,
+          cx * sz,
+          sx * cy * sz - sy * cz,
+          0,
+
+          sx * sy * cz - cy * sz,
+          cx * cz,
+          sx * cy * cz + sy * sz,
+          0,
+
+          cx * sy,
+          -sx,
+          cx * cy,
+          0,
+
+          0,
+          0,
+          0,
+          1
+        );
+        return new Matrix(
+          sx * sy * sz + cy * cz,
           sx * sy * cz - cy * sz,
           cx * sy,
           0,
@@ -105,6 +165,28 @@ export class EulerAngle {
           1
         );
       case EulerOrder.YZX:
+        return new Matrix(
+          cy * cz,
+          sz,
+          -sy * cz,
+          0,
+
+          -cx * cy * sz + sx * sy,
+          cx * cz,
+          cx * sy * sz + sx * cy,
+          0,
+
+          sx * cy * sz + cx * sy,
+          -sx * cz,
+          -sx * sy * sz + cx * cy,
+          0,
+
+          0,
+          0,
+          0,
+          1
+        );
+
         return new Matrix(
           cy * cz,
           -cx * cy * sz + sx * sy,
@@ -129,6 +211,27 @@ export class EulerAngle {
       case EulerOrder.ZXY:
         return new Matrix(
           -sx * sy * sz + cy * cz,
+          sx * sy * cz + cy * sz,
+          -cx * sy,
+          0,
+
+          -cx * sz,
+          cx * cz,
+          sx,
+          0,
+
+          sx * cy * sz + sy * cz,
+          -sx * cy * cz + sy * sz,
+          cx * cy,
+          0,
+
+          0,
+          0,
+          0,
+          1
+        );
+        return new Matrix(
+          -sx * sy * sz + cy * cz,
           -cx * sz,
           sx * cy * sz + sy * cz,
           0,
@@ -149,6 +252,27 @@ export class EulerAngle {
           1
         );
       case EulerOrder.ZYX:
+        return new Matrix(
+          cy * cz,
+          cy * sz,
+          -sy,
+          0,
+
+          sx * sy * cz - cx * sz,
+          sx * sy * sz + cx * cz,
+          sx * cy,
+          0,
+
+          cx * sy * cz + sx * sz,
+          cx * sy * sz - sx * cz,
+          cx * cy,
+          0,
+
+          0,
+          0,
+          0,
+          1
+        );
         return new Matrix(
           cy * cz,
           sx * sy * cz - cx * sz,
@@ -175,7 +299,8 @@ export class EulerAngle {
   }
 
   static toEulerAngle(m: Matrix, order: EulerOrder) {
-    if (order === EulerOrder.XYZ) {
+    // if (order === EulerOrder.XYZ) {
+    if (order === EulerOrder.ZYX) {
       const sy = m.m13; //.at(0, 2);
       const unlocked = Math.abs(sy) < 0.99999;
       return new EulerAngle(
@@ -188,76 +313,81 @@ export class EulerAngle {
         unlocked ? Math.atan2(-m.m12, m.m11) : 0,
         order
       );
-    } else if (order === EulerOrder.XZY) {
+      // } else if (order === EulerOrder.XZY) {
+    } else if (order === EulerOrder.YZX) {
       // const sz = -m.at(0, 1);
-      const sz = -m.m12;
+      const sz = m.m12;
       const unlocked = Math.abs(sz) < 0.99999;
       return new EulerAngle(
         //   unlocked
         //     ? Math.atan2(m.at(2, 1), m.at(1, 1))
         //     : Math.atan2(-m.at(1, 2), m.at(2, 2)),
 
-        unlocked ? Math.atan2(m.m32, m.m22) : Math.atan2(-m.m23, m.m33),
-        unlocked ? Math.atan2(m.m13, m.m11) : 0,
+        unlocked ? Math.atan2(-m.m32, m.m22) : Math.atan2(m.m23, m.m33),
+        unlocked ? Math.atan2(-m.m13, m.m11) : 0,
         Math.asin(sz),
         order
       );
-    } else if (order === EulerOrder.YXZ) {
+      // } else if (order === EulerOrder.YXZ) {
+    } else if (order === EulerOrder.ZXY) {
       // const sx = -m.at(1, 2);
-      const sx = -m.m23;
+      const sx = m.m23;
       const unlocked = Math.abs(sx) < 0.99999;
       return new EulerAngle(
         Math.asin(sx),
         //   unlocked
         //     ? Math.atan2(m.at(0, 2), m.at(2, 2))
         //     : Math.atan2(-m.at(2, 0), m.at(0, 0)),
-        unlocked ? Math.atan2(m.m13, m.m33) : Math.atan2(-m.m31, m.m11),
+        unlocked ? Math.atan2(-m.m13, m.m33) : Math.atan2(m.m31, m.m11),
         //   unlocked ? Math.atan2(m.at(1, 0), m.at(1, 1)) : 0,
-        unlocked ? Math.atan2(m.m21, m.m22) : 0,
+        unlocked ? Math.atan2(-m.m21, m.m22) : 0,
         order
       );
-    } else if (order === EulerOrder.YZX) {
+      // } else if (order === EulerOrder.YZX) {
+    } else if (order === EulerOrder.XZY) {
       // const sz = m.at(1, 0);
-      const sz = m.m21;
+      const sz = -m.m21;
       const unlocked = Math.abs(sz) < 0.99999;
       return new EulerAngle(
         //   unlocked ? Math.atan2(-m.at(1, 2), m.at(1, 1)) : 0,
-        unlocked ? Math.atan2(-m.m23, m.m22) : 0,
+        unlocked ? Math.atan2(m.m23, m.m22) : 0,
         //   unlocked
         //     ? Math.atan2(-m.at(2, 0), m.at(0, 0))
         //     : Math.atan2(m.at(0, 2), m.at(2, 2)),
 
-        unlocked ? Math.atan2(-m.m31, m.m11) : Math.atan2(m.m13, m.m33),
+        unlocked ? Math.atan2(m.m31, m.m11) : Math.atan2(-m.m13, m.m33),
         Math.asin(sz),
         order
       );
-    } else if (order === EulerOrder.ZXY) {
+      // } else if (order === EulerOrder.ZXY) {
+    } else if (order === EulerOrder.YXZ) {
       // const sx = m.at(2, 1);
-      const sx = m.m32;
+      const sx = -m.m32;
       const unlocked = Math.abs(sx) < 0.99999;
       return new EulerAngle(
         Math.asin(sx),
         //   unlocked ? Math.atan2(-m.at(2, 0), m.at(2, 2)) : 0,
-        unlocked ? Math.atan2(-m.m31, m.m33) : 0,
+        unlocked ? Math.atan2(m.m31, m.m33) : 0,
         //   unlocked
         //     ? Math.atan2(-m.at(0, 1), m.at(1, 1))
         //     : Math.atan2(m.at(1, 0), m.at(0, 0)),
 
-        unlocked ? Math.atan2(-m.m12, m.m22) : Math.atan2(m.m21, m.m11),
+        unlocked ? Math.atan2(m.m12, m.m22) : Math.atan2(-m.m21, m.m11),
         order
       );
-    } else if (order === EulerOrder.ZYX) {
+      // } else if (order === EulerOrder.ZYX) {
+    } else if (order === EulerOrder.XYZ) {
       // const sy = -m.at(2, 0);
-      const sy = -m.m31;
+      const sy = m.m31;
       const unlocked = Math.abs(sy) < 0.99999;
       return new EulerAngle(
         //   unlocked ? Math.atan2(m.at(2, 1), m.at(2, 2)) : 0,
-        unlocked ? Math.atan2(m.m32, m.m33) : 0,
+        unlocked ? Math.atan2(-m.m32, m.m33) : 0,
         Math.asin(sy),
         //   unlocked
         //     ? Math.atan2(m.at(1, 0), m.at(0, 0))
         //     : Math.atan2(-m.at(0, 1), m.at(1, 1)),
-        unlocked ? Math.atan2(m.m21, m.m11) : Math.atan2(-m.m12, m.m22),
+        unlocked ? Math.atan2(-m.m21, m.m11) : Math.atan2(m.m12, m.m22),
         order
       );
     }
--- tests/eulerAngle.test.ts
+++ tests/eulerAngle.test.ts
@@ -1,9 +1,54 @@
 import { describe, expect, it, test } from 'vitest';
 
-import { EulerAngle, EulerOrder, MathHelper } from '../src';
+import { EulerAngle, EulerOrder, MathHelper, Matrix } from '../src';
 import { getRandomFloat } from './utils';
 
 describe('EulerAngle', () => {
+  it('RotationX', () => {
+    for (let i = 0; i < EulerOrder.ZYX; ++i) {
+      const x = MathHelper.ToRadians(getRandomFloat(-180, 180));
+      const y = 0;
+      const z = 0;
+      const m1 = Matrix.RotationX(x);
+
+      const eulerAngle = new EulerAngle(x, y, z, i);
+
+      const m2 = EulerAngle.toRotationMatrix(eulerAngle);
+
+      expect(m1.equalsWithEpsilon(m2)).toBeTruthy();
+    }
+  });
+
+  it('RotationY', () => {
+    for (let i = 0; i < EulerOrder.ZYX; ++i) {
+      const x = 0;
+      const y = MathHelper.ToRadians(getRandomFloat(-180, 180));
+      const z = 0;
+      const m1 = Matrix.RotationY(y);
+
+      const eulerAngle = new EulerAngle(x, y, z, i);
+
+      const m2 = EulerAngle.toRotationMatrix(eulerAngle);
+
+      expect(m1.equalsWithEpsilon(m2)).toBeTruthy();
+    }
+  });
+
+  it('RltationZ', () => {
+    for (let i = 0; i < EulerOrder.ZYX; ++i) {
+      const x = 0;
+      const y = 0;
+      const z = MathHelper.ToRadians(getRandomFloat(-180, 180));
+      const m1 = Matrix.RotationZ(z);
+
+      const eulerAngle = new EulerAngle(x, y, z, i);
+
+      const m2 = EulerAngle.toRotationMatrix(eulerAngle);
+
+      expect(m1.equalsWithEpsilon(m2)).toBeTruthy();
+    }
+  });
+
   it('EulerOrder', () => {
     for (let i = 0; i < EulerOrder.ZYX; ++i) {
       const x = MathHelper.ToRadians(getRandomFloat(-180, 180));
@@ -19,7 +64,8 @@ describe('EulerAngle', () => {
       const m2 = EulerAngle.toRotationMatrix(e1);
 
       const e2 = EulerAngle.toEulerAngle(m2, eulerAngle.order);
-
+      //   expect(m1).toEqual(m2);
+      //   expect(e1).toEqual(e2);
       expect(m1.equalsWithEpsilon(m2)).toBeTruthy();
       expect(e1.equalsWithEpsilon(e2)).toBeTruthy();
     }
